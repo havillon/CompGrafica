34240a41811fb3920a0c03d60215ca7d930bfe2b
havillon <havillon@alu.ufc.br>
2022-12-08 12:59:14 -0300

    canvas getters e setters

--- include/canvas.h
+++ include/canvas.h
@@ -1,12 +1,26 @@
 #pragma once
 
 class Canvas {
-public:
+
+private:
     int wJanela;
     int hJanela;
     int nLinhas;
     int nColunas;
-    
+
+public: 
     Canvas();
     Canvas(int wJanela, int hJanela, int nLinhas, int nColunas);
+
+    int getWJanela();
+    void setWJanela(int wJanela);
+
+    int getHJanela();
+    void setHJanela(int hJanela);
+
+    int getNLinhas();
+    void setNLinhas(int nLinhas);
+    
+    int getNColunas();
+    void setNColunas(int nColunas);
 };
--- lib/canvas.cpp
+++ lib/canvas.cpp
@@ -8,3 +8,31 @@ Canvas::Canvas(int wJanela, int hJanela, int nLinhas, int nColunas){
   this->nLinhas = nLinhas;
   this->nColunas = nColunas;
 }
+
+int Canvas::getWJanela(){
+  return this->wJanela;
+}
+void Canvas::setWJanela(int wJanela){
+  this->wJanela = wJanela; 
+}
+
+int Canvas::getHJanela(){
+  return this->hJanela;
+}
+void Canvas::setHJanela(int hJanela){
+  this->hJanela = hJanela; 
+}
+
+int Canvas::getNLinhas(){
+  return this->nLinhas;
+}
+void Canvas::setNLinhas(int nLinhas){
+  this->nLinhas = nLinhas; 
+}
+
+int Canvas::getNColunas(){
+  return this->nColunas;
+}
+void Canvas::setNColunas(int nColunas){
+  this->nColunas = nColunas; 
+}
